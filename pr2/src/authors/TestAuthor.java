2bb526a040ab9eaf8e2bce0195336556af6d9e7c
Artem Antonov <artemon939@gmail.com>
2024-09-12 15:26:26 +0300

    Add files via upload

---
+++ pr2/src/authors/TestAuthor.java
@@ -0,0 +1,8 @@
+package authors;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a1 = new Author("Vasya", "Vasua228@mail.ru", 'M');
+        System.out.println(a1);
+    }
+}
